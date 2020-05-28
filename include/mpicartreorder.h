ce2b4f3b089cca6cd8006919dd31130435c31ce5
Konrad von Kirchbach <konradvonkirchbach@gmail.com>
2020-05-28 15:17:40 +0200

    Added MPI Header to mpicartreorder.h

--- include/mpicartreorder.h
+++ include/mpicartreorder.h
@@ -1,6 +1,9 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#include <mpi.h>
+
 int MPIX_Cart_comm_stencil(MPI_Comm comm_old, int ndims, const int dims[],
 						   const int periods[], int reorder, const int stencil[],
 						   const int n_neighbors, MPI_Comm *comm_cart);
