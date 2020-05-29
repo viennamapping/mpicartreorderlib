becfd5c9c2dec3d65fdab50aac70d070ba24b0b9
Konrad von Kirchbach <konradvonkirchbach@gmail.com>
2020-05-29 11:09:16 +0200

    Further debugging

--- src/Evaluation/evaluation_methods.h
+++ src/Evaluation/evaluation_methods.h
@@ -12,6 +12,19 @@
 #include "configuration/configuration.h"
 #include "Stencils/Stencil_Creater.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+int MPIX_Cart_comm_stencil(MPI_Comm comm_old, int ndims, const int dims[],
+						   const int periods[], int reorder, const int stencil[],
+						   const int n_neighbors, MPI_Comm *comm_cart);
+
+void MPIX_Internode_cost_stencil(MPI_Comm cart_comm, int* total, int* max, int stencil[], int n_neighbors);
+void MPIX_Internode_cost(MPI_Comm cart_comm, int* total, int* max);
+#ifdef __cplusplus
+}
+#endif
+
 namespace mpireorderinglib {
 void get_neighbors_from_stencil(MPI_Comm cart_comm,
 									  const int cart_rank,
--- src/configuration/configuration.h
+++ src/configuration/configuration.h
@@ -14,6 +14,19 @@
 #include "Stencils/Stencil_Creater.h"
 #include "spdlog/spdlog.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+int MPIX_Cart_comm_stencil(MPI_Comm comm_old, int ndims, const int dims[],
+						   const int periods[], int reorder, const int stencil[],
+						   const int n_neighbors, MPI_Comm *comm_cart);
+
+void MPIX_Internode_cost_stencil(MPI_Comm cart_comm, int* total, int* max, int stencil[], int n_neighbors);
+void MPIX_Internode_cost(MPI_Comm cart_comm, int* total, int* max);
+#ifdef __cplusplus
+}
+#endif
+
 namespace mpireorderinglib {
 class Configuration {
 private:
